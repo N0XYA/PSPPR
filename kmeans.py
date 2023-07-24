1e2a3cdea17ff45eed676cdaab31c616606ace18
Kirill <kpicmo@mail.ru>
2023-07-24 18:21:02 +0400

    added create_obj func

--- kmeans.py
+++ kmeans.py
@@ -1,1 +1,19 @@
-print("Hi VSCode!")
+import random
+
+
+xrange = 10
+yrange = 10
+def create_obj(xrange, yrange):
+    object = []
+    len = random.randint(0, 10)
+    print(len)
+    for i in range(0, len):
+        coord = [random.randint(-xrange, xrange), random.randint(-yrange, yrange)]
+        object.append(coord)
+    return object
+
+obj = create_obj(xrange, yrange)
+
+for coord in obj:
+    print(coord)
+
