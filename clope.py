f44c8fbd9a991e58dd46e862a83b7f10c0141d42
Kirill <kpicmo@mail.ru>
2023-08-07 13:10:27 +0400

    still trying

--- clope.py
+++ clope.py
@@ -3,36 +3,41 @@ transactions = [('a','b'), ('a', 'b', 'c'),
                 ('d', 'e', 'f')]
 
 first_cluster = [[('a','b'), ('a', 'b', 'c'), ('a', 'c', 'd')],
-                [('d', 'e', 'f')]]
+                [('d', 'e'), ('d', 'e', 'f')]]
 
 second_cluster = [[('a','b'), ('a', 'b', 'c')],
                 [('a', 'c', 'd'), ('d', 'e'), ('d', 'e', 'f')]]
 
 
 
-def count_e(transactions):
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    print(transactions)
+def find_unique_items(transactions):
+    # uniques = {}
+    uniques = []
     for elements in transactions:
-        # print(elements)
-        pass
-        # a += transactions.count('a')
-        # b += transactions.count('b')
-        # c += transactions.count('c')
-        # d += transactions.count('d')
-
-    return([a, b, c, d])
+        for item in elements:
+            if item not in uniques:
+                # uniques[item] = 0
+                uniques.append(item)
+    return(uniques)
 
 
-def count_entries(cluster):
-    value = []
-    for elements in cluster:
-        for transactions in elements:
-            value.append(count_e(transactions))
-    return(value)
+def count_items(cluster, unique_items):
+    for transactions in cluster:
+        uniques = []
+        count = 0
+        for elements in transactions:
+            print(elements)
+            for unique in unique_items:
+                if unique in elements:
+                    count += 1
+                    if unique not in uniques:
+                        uniques.append(unique)
+        print(count, len(uniques))
+        uniques.clear
+    return
 
-value = count_entries(first_cluster)
-print(value)
+unique_items = find_unique_items(transactions)
+count_items(first_cluster, unique_items)
+
+# value = count_entries(first_cluster)
+# print(value)
