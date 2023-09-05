09816acb9a94a60b8ad13ec0c1551845b3c7b5cc
Kirill <kpicmo@mail.ru>
2023-09-05 11:38:21 +0300

    started lin regression!

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from decimal import Decimal
+import math
 import matplotlib.pyplot as plt
 
 
@@ -7,12 +9,26 @@ def regression(coordinates):
     x.insert(0, coordinates.columns.values.tolist()[0])
     y = [coordinate[1] for coordinate in coordinates.values]
     y.insert(0, coordinates.columns.values.tolist()[1])
-    print(x)
-    xmean = sum(x)
-    ymean = sum(y)
-    print(xmean, ymean)
-    return
+    LENGTH = len(x)
+    xmean = sum(x) / LENGTH
+    ymean = sum(y) / LENGTH
+    xmean = round(xmean, 1)
+    xerr = []
+    for i in range(len(x)):
+        xi = float(x[i])
+        value = xi - xmean
+        value = round(value, 1)
+        print(value)
+        xerr.append(value)
+    sum_x_err = 0
+    for value in xerr:
+        sum_x_err+=value
+    
+    print(sum_x_err)
 
+
+
+    return
 def main():
 
     df = pd.read_excel('lingres_xy.xlsx')
